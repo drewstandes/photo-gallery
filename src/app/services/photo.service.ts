816b652bf393f8961a021a6ce580f2b94aaed5ce
Andrew <drewstandes@gmail.com>
2023-09-22 11:29:08 +0100

    Saving photos to filesystem

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -21,13 +21,47 @@ export class PhotoService {
     });
 
 
-    this.photos.unshift({
-      filepath: "soon...",
-      webviewPath: capturedPhoto.webPath!
-    });
+    const savedImageFile = await this.savePicture(capturedPhoto)
+    this.photos.unshift(savedImageFile);
 
-    console.log(this.photos);
   }
+
+  private async savePicture(photo: Photo) {
+    // Convert photo to base64 format, required by Filesystem API to save
+    const base64Data = await this.readAsBase64(photo);
+  
+    // Write the file to the data directory
+    const fileName = Date.now() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data
+    });
+  
+    // Use webPath to display the new image instead of base64 since it's
+    // already loaded into memory
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath
+    };
+  }
+
+  private async readAsBase64(photo : Photo) {
+      // Fetch the photo, read as a blob, then convert to base64 format
+      const response = await fetch(photo.webPath!);      // log
+      const blob = await response.blob();
+
+      return await this.convertBlobToBase64(blob) as string;
+  }
+
+  private convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onerror = reject;
+    reader.onload = () => {
+        resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
 }
 
 export interface UserPhoto {
